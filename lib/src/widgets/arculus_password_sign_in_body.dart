5a021664f4fc4553f6548467cb4acf93e5e67e78
Moses Karunia <moseskarunia@gmail.com>
2021-01-28 16:41:17 +0700

    username field

---
+++ lib/src/widgets/arculus_password_sign_in_body.dart
@@ -0,0 +1,185 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+/// Predefined passowrd sign in body which can be easily implemented.
+/// Will automatically manages interaction such as disabling text fields when
+/// loading is processed, and disabling sign in button once tapped to prevent
+/// multiple triggers.
+class ArculusPasswordSignInBody extends StatefulWidget {
+  /// Controller which contains the functions to be used in sign in body.
+  final ArculusSignInController controller;
+
+  /// Contains static UI-related data such as label, hints, etc.
+  final ArculusSignInStaticUIData uiData;
+
+  /// Provide sign in state to modify the UI, such as errorMessage and isLoading
+  final ArculusSignInState signInState;
+
+  const ArculusPasswordSignInBody({
+    Key key,
+    @required this.controller,
+    @required this.uiData,
+    this.signInState,
+  }) : super(key: key);
+
+  @override
+  _ArculusPasswordSignInBodyState createState() =>
+      _ArculusPasswordSignInBodyState();
+}
+
+class _ArculusPasswordSignInBodyState extends State<ArculusPasswordSignInBody> {
+  TextEditingController _usernameController;
+  TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _usernameController =
+        TextEditingController(text: widget.signInState.username ?? '');
+    _passwordController =
+        TextEditingController(text: widget.signInState.username ?? '');
+  }
+
+  @override
+  void dispose() {
+    _usernameController?.dispose();
+    _passwordController?.dispose();
+    super.dispose();
+  }
+
+  Future<void> _signIn(BuildContext context) async {
+    /// TODO: Hide keyboard
+
+    // final result = await widget.controller.onSignInPressed(
+    //   context,
+    //   _usernameController.text,
+    //   _passwordController.text,
+    // );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      children: [
+        Container(
+          margin: const EdgeInsets.only(bottom: 16),
+          child: TextField(
+            key: Key('sign-in-username'),
+            controller: _usernameController,
+            enabled: !widget.signInState.isLoading,
+            decoration: InputDecoration(hintText: widget.uiData.usernameHint),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(bottom: 32),
+          child: TextField(
+            key: Key('sign-in-password'),
+            controller: _passwordController,
+            decoration: InputDecoration(hintText: widget.uiData.passwordHint),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(bottom: 16),
+          child: ElevatedButton(
+            key: Key('sign-in-button'),
+            child: Text(widget.uiData.signInButtonLabel),
+            onPressed: () => _signIn(context),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+/// State of the sign in attempt. It can contains errorMessage, and Session.
+/// [errorMessage] determines if the state is `isAuthenticated` or not.
+///
+/// The arculus sign in body will automatically displays the error message
+/// at the bottom of sign in button if provided, and prevents it from calling
+/// [onAuthenticated].
+class ArculusSignInState extends Equatable {
+  /// Error message from the result of sign in attempt.
+  final String errorMessage;
+
+  /// If true, the button and text field won't be interactable.
+  final bool isLoading;
+
+  /// Value of username and password. You can use this to show the user if
+  /// there's any cached initial value.
+  final String username, password;
+
+  const ArculusSignInState({
+    this.errorMessage,
+    this.isLoading = false,
+    this.username,
+    this.password,
+  });
+
+  @override
+  List<Object> get props => [errorMessage, isLoading, username, password];
+}
+
+/// Controller which contains callbacks pf arculus sign in body.
+abstract class ArculusSignInController {
+  /// Triggered when sign in button is tapped.
+  Future<ArculusSignInState> onSignInPressed(
+    BuildContext context,
+    String username,
+    String password,
+  );
+
+  /// Triggered when the [forgotPasswordActionLabel] is pressed.
+  Future<void> onForgotPasswordPressed(BuildContext context);
+
+  /// Triggered when the [registerActionLabel] is pressed.
+  Future<void> onRegisterPressed(BuildContext context);
+}
+
+/// Static UI data of arculus sign in body.
+class ArculusSignInStaticUIData {
+  /// Hint of the username field. e.g. "email", "username", "phoneNumber", etc.
+  final String usernameHint;
+
+  /// Hint of the password field.
+  final String passwordHint;
+
+  /// Label of the sign in button. Will trigger [onSignInPressed] when tapped.
+  final String signInButtonLabel;
+
+  /// Question on the forgot password label. This label is not interactable,
+  /// just to make the message more friendly. The interactable label is
+  /// [forgotPasswordActionLabel].
+  ///
+  /// If null or empty, forgot password option is not displayed
+  final String forgotPasswordQuestionLabel;
+
+  /// Action label which will be placed after [forgotPasswordQuestionLabel].
+  /// Will trigger [onForgotPasswordPressed].
+  ///
+  /// If null or empty, forgot password option is not displayed
+  final String forgotPasswordActionLabel;
+
+  /// Question on the register label. This label is not interactable,
+  /// just to make the message more friendly. The interactable label is
+  /// [registerActionLabel].
+  ///
+  /// If null or empty, register option is not displayed
+  final String registerQuestionLabel;
+
+  /// Action label which will be placed after [registerQuestionLabel].
+  /// Will trigger [onRegisterPressed] when pressed.
+  ///
+  /// If null or empty, register option is not displayed
+  final String registerActionLabel;
+
+  const ArculusSignInStaticUIData({
+    @required this.usernameHint,
+    @required this.passwordHint,
+    @required this.signInButtonLabel,
+    this.forgotPasswordActionLabel,
+    this.registerActionLabel,
+    this.forgotPasswordQuestionLabel,
+    this.registerQuestionLabel,
+  });
+}
---
+++ test/src/widgets/arculus_password_sign_in_body_test.dart
@@ -0,0 +1,162 @@
+import 'package:arculus/src/widgets/arculus_password_sign_in_body.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockController extends Mock implements ArculusSignInController {}
+
+void main() {
+  MockController controller;
+
+  setUp(() {
+    controller = MockController();
+  });
+
+  group('ArculusSignInState', () {
+    test('props', () {
+      final state = ArculusSignInState(
+        username: 'test@test.com',
+        password: 'admin12345',
+        errorMessage: 'Invalid username',
+        isLoading: true,
+      );
+      expect(state.props, [
+        'Invalid username',
+        true,
+        'test@test.com',
+        'admin12345',
+      ]);
+    });
+    testWidgets(
+      'should assign provided username and password state to their '
+      'respective text fields',
+      (tester) async {},
+    );
+    testWidgets(
+      'should display provided errorMessage if provided',
+      (tester) async {},
+    );
+    testWidgets(
+      'when isLoading, should disable text fields and display loading indicator',
+      (tester) async {},
+    );
+    testWidgets(
+      'when ',
+      (tester) async {},
+    );
+  });
+
+  group('username TextField', () {
+    final uiDataFixture = ArculusSignInStaticUIData(
+      usernameHint: 'Email or username',
+      passwordHint: 'Password',
+      signInButtonLabel: 'SIGN IN',
+    );
+
+    testWidgets(
+      'should be wrapped in container with 16 bottom margin',
+      (tester) async {
+        final stateFixture = ArculusSignInState();
+
+        await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+            body: ArculusPasswordSignInBody(
+              controller: controller,
+              uiData: uiDataFixture,
+              signInState: stateFixture,
+            ),
+          ),
+        ));
+
+        final usernameFieldFinder = find.byKey(Key('sign-in-username'));
+        final containerWrapperFinder = find
+            .ancestor(of: usernameFieldFinder, matching: find.byType(Container))
+            .first;
+
+        expect(
+          tester.widget<Container>(containerWrapperFinder).margin,
+          EdgeInsets.only(bottom: 16),
+        );
+      },
+    );
+    testWidgets(
+      'should be empty with provided hint when no username provided in state',
+      (tester) async {
+        final stateFixture = ArculusSignInState();
+
+        await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+            body: ArculusPasswordSignInBody(
+              controller: controller,
+              uiData: uiDataFixture,
+              signInState: stateFixture,
+            ),
+          ),
+        ));
+
+        final usernameFieldFinder = find.byKey(Key('sign-in-username'));
+        final usernameTextField = tester.widget<TextField>(usernameFieldFinder);
+
+        expect(
+          usernameTextField.decoration.hintText,
+          uiDataFixture.usernameHint,
+        );
+        expect(usernameTextField.controller.text, '');
+      },
+    );
+
+    testWidgets(
+      'should assign state.username if provided',
+      (tester) async {
+        final stateFixture = ArculusSignInState(username: 'test@test.com');
+
+        await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+            body: ArculusPasswordSignInBody(
+              controller: controller,
+              uiData: uiDataFixture,
+              signInState: stateFixture,
+            ),
+          ),
+        ));
+
+        final usernameFieldFinder = find.byKey(Key('sign-in-username'));
+        final usernameTextField = tester.widget<TextField>(usernameFieldFinder);
+
+        expect(
+          usernameTextField.decoration.hintText,
+          uiDataFixture.usernameHint,
+        );
+        expect(usernameTextField.controller.text, 'test@test.com');
+      },
+    );
+
+    testWidgets('should not be editable when state.isLoading', (tester) async {
+      final stateFixture = ArculusSignInState(
+        username: 'test@test.com',
+        password: 'admin123',
+        isLoading: true,
+      );
+
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: ArculusPasswordSignInBody(
+            controller: controller,
+            uiData: uiDataFixture,
+            signInState: stateFixture,
+          ),
+        ),
+      ));
+
+      final usernameFieldFinder = find.byKey(Key('sign-in-username'));
+      final usernameTextField = tester.widget<TextField>(usernameFieldFinder);
+
+      expect(
+        usernameTextField.decoration.hintText,
+        uiDataFixture.usernameHint,
+      );
+      expect(usernameTextField.controller.text, 'test@test.com');
+      expect(usernameTextField.enabled, false);
+    });
+  });
+}
